d9d155a4d40b7144dbf21dfaf052247f6866a7ca
Iago <iagoqo@users.noreply.github.com>
2015-12-05 17:25:51 +0100

    Added vote functionality and show total votes next to the arrows.

--- client/siteace.js
+++ client/siteace.js
@@ -24,34 +24,42 @@ Template.website_list.helpers({
 
 Template.website_item.events({
 	"click .js-upvote":function(event){
-		// example of how you can access the id for the website in the database
-		// (this is the data context for the template)
-		var website_id = this._id;
-		console.log("Up voting website with id "+website_id);
-		// put the code in here to add a vote to a website!
 
-		return false;// prevent the button from reloading the page
+		var website_id = this._id;
+
+		// Add one vote
+		if (Meteor.user()) {
+			Websites.update(
+				{_id:website_id},
+				{$inc:{votes:1}}
+			);
+		}
+		
+		// Prevent page reload
+		return false;
 	},
 	"click .js-downvote":function(event){
 
-		// example of how you can access the id for the website in the database
-		// (this is the data context for the template)
 		var website_id = this._id;
-		console.log("Down voting website with id "+website_id);
 
-		// put the code in here to remove a vote from a website!
-
+		// Remove one vote
+		if (Meteor.user()) {
+			Websites.update(
+				{_id:website_id},
+				{$inc:{votes:-1}}
+			);
+		}
+		
 		return false;// prevent the button from reloading the page
 	}
-	})
+});
 
-	Template.website_form.events({
+Template.website_form.events({
 	"click .js-toggle-website-form":function(event){
 		$("#website_form").toggle('slow');
 	},
 	"submit .js-save-website-form":function(event){
-
-		// here is an example of how to get the url out of the form:
+		
 		var url = event.target.url.value;
 		var title = event.target.title.value;
 		var description = event.target.title.value;
@@ -62,11 +70,17 @@ Template.website_item.events({
 				title:title,
 				description:description,
 				createdOn:new Date(),
-				createdBy:Meteor.user()._id
+				createdBy:Meteor.user()._id,
+				votes:0
 			});
 		}
 		
-		return false;// stop the form submit from reloading the page
-
+		// Hide the form div
+		$("#website_form").toggle('slow');
+		// Reset the form fields
+		$("#website_form").find("form")[0].reset();
+		
+		// Prevent page reload
+		return false;
 	}
 });
